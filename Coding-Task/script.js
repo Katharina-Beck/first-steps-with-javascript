12d0759873dbcf6f854076dd89f5c174c168f717
Katharina-Beck <katja131291@gmail.com>
2023-11-23 16:57:24 +0100

    solved task  oparetors & data types

--- Coding-Task/script.js
+++ Coding-Task/script.js
@@ -5,11 +5,11 @@ console.log("K" + "A" + "T" + "J" + "A");
 // Error! Make it work
 console.log("Hallo");
 // Error! Make it work
-console.log("That doesnt work");
+console.log("That doesn`t work");
 // should return true. Change only the operator to fix it.
-console.log(50 + 2 == 52);
+console.log(50 + 2 === 52);
 // should be false. Change only the operator to fix it.
-console.log("333" != 333);
+console.log("333" === 333);
 // Wrong result: Expected 555 not 855. Change only the operator to fix it.
 console.log((600 + 510) / 2);
 // Wrong result: Expected a Number 4 received 22
@@ -17,3 +17,11 @@ console.log(2 + 2);
 
 console.log(25 / 5 === 5); // Result: 5
 console.log(50 / 10 === 5); // Result: 5
+
+console.log((50 / 10 === 5 && 50 / 10 === 5);
+console.log((50 / 10 === 5) && (50 / 10 === 5));
+console.log(5 && 5 === 5);
+console.log(5 && true);
+
+console.log("a" && "abc")
+console.log("a"  )
